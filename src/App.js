bdb13b97cf25c6d4d9d151fc08588199743724a3
VitorCdSouza <vitorcorddesouza@gmail.com>
2024-09-13 17:16:56 -0300

    dividing screen for img and future form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React from "react";
+import background from "./assets/flanker-lee-naeD-4ShjG0-unsplash.jpg";
+import Login from "./components/Login";
+
 function App() {
   return (
-    <div className="flex container">
-       
+    <div className="flex h-screen">
+      {/* bg at left */}
+      <div
+        className="basis-9/12 h-full bg-cover bg-center"
+        style={{ backgroundImage: `url(${background})` }}
+      >
+      </div>
+
+      {/* login at right */}
+      <div className="basis-3/12 flex items-center justify-center bg-white">
+        <Login />
+      </div>
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <div className="w-full max-w-md p-8 space-y-6">
+            
+        </div>
+    );
+};
+
+export default Login;
